a06c100764194648835ef3862d412ea8ec576acf
jenna0315 <jbellagamba@usf.edu>
2024-09-08 13:35:54 -0400

    Update bill_splitter.js

--- bill_splitter.js
+++ bill_splitter.js
@@ -1,6 +1,6 @@
 //Initial Logic for Calculating the Tip
-var customerBill = 295 //number
-var customertip = (300 < customerBill < 50) ? (.2*customerBill) : (.15*customerBill) 
+let customerBill = 295 //number
+let customertip = (300 < customerBill < 50) ? (.2*customerBill) : (.15*customerBill) 
 console.log("Customer Tip:",customertip)
 
 //Add output functionality to display bill, tip, and total
@@ -11,9 +11,12 @@ The tip was $${customertip}
 The total was $${customerTotal}`
 console.log(customerSummary)
 
-//Create and test calculatecustomerTip function
+//Create and test calculateTip function
 let Bill = 100
-function calculatecustomerTip(Bill,tip){return Bill+(300 < Bill < 50) ? (.2*Bill) : (.15*Bill)}
+function calculateTip(Bill,tip){return Bill+(300 < Bill < 50) ? (.2*Bill) : (.15*Bill)}
 let  tip = (300 < Bill < 50) ? (.2*Bill) : (.15*Bill)
 console.log("Customer Bill:",Bill)
-console.log("Customer Tip:",calculatecustomerTip(Bill,tip))
+console.log("Customer Tip:",calculateTip(Bill,tip))
+
+//Implement and populate arrays for bills, tips, and totals
+let bills = {275, 40, 430}
